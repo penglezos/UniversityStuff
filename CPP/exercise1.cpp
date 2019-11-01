b055b352549750bcb1fd4f3f262318caa0ac821e
penglezos <panagiotisegl@gmail.com>
2019-11-01 19:48:12 +0200

    C++ exercise 1

---
+++ CPP/exercise1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int number = 1;
+    int sum = 0;
+
+    while(number!=0)
+    {
+        cout<<"ΔΩΣΕ ΑΡΙΘΜΟ:"
+        cin>>number;
+        sum= sum + number;
+    }
+
+    cout <<"ΑΘΡΟΙΣΜΑ:"<< ΣΘΜ
+    return 0;
+}
