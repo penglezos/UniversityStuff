a39dc90a23062a03cd7f2cadc1d2a84c3bded0b2
Panagiotis Englezos <panagiotisegl@gmail.com>
2019-11-08 13:26:42 +0200

    Exercise 8

---
+++ CPP/exercise8.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+struct fraction {
+	int arithmitis;
+	int paronomastis;
+};
+
+void print(fraction f) {
+	cout<<f.arithmitis<<"/"<<f.paronomastis<<endl;
+}
+
+fraction add(fraction f1, fraction f2) {
+	fraction f3;
+	f3.arithmitis=f1.arithmitis*f2.paronomastis+f1.paronomastis*f2.a
+}
