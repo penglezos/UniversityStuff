e9a43c11c0af900df67e35b4d7a27b4880133168
Panagiotis Englezos <panagiotisegl@gmail.com>
2019-11-15 11:54:13 +0200

    exercise 9

---
+++ CPP/exercise9.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+struct time {
+	int seconds;
+	int minutes;
+	int hours;
+};
+
+void init(time *t) {
+	cout<<"Dose ores lepta kai deuterolepta:"<< endl;
+	cin>> t -> hours>> t -> minutes>> t -> seconds;
+}
+
+void print(time t) {
+	cout<<t.hours<<":"<<t.minutes<<":"<<t.seconds<<endl;
+}
+
+int toSeconds(time t){
+	return t.hours * 3600 + t.minutes * 60 + t.seconds;
+}
+
+void validate(time *t) {
+	if(t -> hours < 0 && t -> hours > 24) {
+		t -> hours = 0;
+		cout<<"ektos orion oi ores eginan 0"<<endl;
+	}
+	if (t -> minutes < 0 && t -> minutes > 60) {
+		t -> minutes = 0;
+		cout<<"......"<<endl;
+	}
+	if (t -> seconds < 0 && t -> seconds>60) {
+		t -> seconds = 0;
+		cout <<"...."<< endl;
+	}
+}
+
+int main() {
+	time t1,t2;
+	int s;
+	
+	init(&t1);
+	init(&t2);
+	
+	print(t1);
+	print(t2);
+	s = toSeconds(t1);
+	cout <<" o xronos";
+	print(t1);
+	cout<<"se deuterolepta einai: "<<s<<endl;
+	
+	validate(&t1);
+	print(t1);
+	return 0;
+}
+
