5322ef458feee109c53bf8fa573945e5e6feb7a5
Panagiotis Englezos <panagiotisegl@gmail.com>
2019-11-18 10:11:47 +0200

    grdate function

--- PHP/index2.php
+++ PHP/index2.php
@@ -12,6 +12,30 @@
                         "onoma" => "Stefanos",
                         "email" => "sts@in.gr",
                         "age" => 21);
+        foreach($student as $pointer => $value) {
+            echo "<p>".$pointer." = ".$value."</p>";
+        }
+    ?>
+    <p>Κωδικός: <?=$student["id"]?></p>
+    <p>Επώνυμο: <?=$student["eponimo"]?></p>
+    <p>Όνομα: <?=$student["onoma"]?></p>
+    <p>Email: <?=$student["email"]?></p>
+    <p>Ηλικία: <?=$student["age"]?></p>
+
+    <h2>ΜΕΘΟΔΟΙ ΣΤΗΝ PHP</h2>
+    <?php
+        function test() {
+            echo "Η πρώτη μου συνάρτηση!!!";
+        }
+        test();
+
+        function grdate() {
+            $dates = array("Κυριακή","Δευτέρα", "Τρίτη", "Τετάρτη", "Πέμπτη", "Παρασκευή", "Σάββατο");
+            $day = date("w",time());
+            $month = date("n",time())-1;
+            echo"<p>".$dates[$day]." ".date("d/m/Y",time())."</p>";
+        }
+        grdate();
     ?>
     </body>
 </html>
