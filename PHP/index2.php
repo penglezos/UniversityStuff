4f0af913da1fe4a6a86e839c8704e74c147292e5
Panagiotis Englezos <panagiotisegl@gmail.com>
2019-11-18 09:22:20 +0200

    Exercise 2

---
+++ PHP/index2.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>PHP</title>
+		<meta charset="utf-8"/>
+	</head>
+	<body>
+    <h2>ΠΙΝΑΚΕΣ ΜΕ ΔΕΙΚΤΕΣ ΚΑΙ ΟΝΟΜΑΤΑ</h2>
+    <?php
+        $student = array("id" => 789654,
+                        "eponimo" => "Tsitsipas",
+                        "onoma" => "Stefanos",
+                        "email" => "sts@in.gr",
+                        "age" => 21);
+    ?>
+    </body>
+</html>
