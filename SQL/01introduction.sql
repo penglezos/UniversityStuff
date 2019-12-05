fa7707490e9b8b44d53c50b840a6cfefde7e4b37
Panagiotis Englezos <panagiotisegl@gmail.com>
2019-12-05 09:55:36 +0200

    Update 01introduction.sql

--- SQL/01introduction.sql
+++ SQL/01introduction.sql
@@ -3,3 +3,9 @@ SELECT * FROM tmhmata;
 SELECT tmimata_onoma FROM tmhmata;
 
 SELECT tmimata_id, tmimata_onoma FROM tmhmata;
+
+SELECT tmimata_id AS KODIKOS_TMIMATOS,
+	tmimata_onoma AS ONOMA_TMIMATOS,
+    tmimata_sindromi AS SINDROMI_TMIMATOS
+   
+FROM tmhmata
