53d22e5cc1d58fa2f21e50a9b7172ff55e100ea5
Panagiotis Englezos <panagiotisegl@gmail.com>
2019-12-06 11:08:01 +0200

    Classes intro update

--- CPP/classes_intro.cpp
+++ CPP/classes_intro.cpp
@@ -2,13 +2,25 @@
 using namespace std;
 
 // Klasesis // 
+/*
 class classname {
-	// private:
-		//metavlites
-		//int data1;
-		//float data2;
-	//public: 
-	//synartisis
-	//void memberfunc1(int d);
-	//void memberfunc2(float x);
+	private:
+		metavlites
+		int data1;
+		float data2;
+	public: 
+		synartisis
+		void memberfunc1(int d);
+		void memberfunc2(float x);
 };
+
+Test::memberfunc1(int d) {
+	data1 = d;
+}
+
+Test::memberfunc2(float x) {
+	data2 = x;
+}
+*/
+
+
