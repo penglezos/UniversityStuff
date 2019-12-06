7f792ce78c86e38145718bece5d695aadedf4a05
Panagiotis Englezos <panagiotisegl@gmail.com>
2019-12-06 10:41:47 +0200

    Introduction to classes

---
+++ CPP/classes_intro.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+// Klasesis // 
+class classname {
+	// private:
+		//metavlites
+		//int data1;
+		//float data2;
+	//public: 
+	//synartisis
+	//void memberfunc1(int d);
+	//void memberfunc2(float x);
+};
