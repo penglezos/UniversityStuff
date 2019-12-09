c7bcd3097f8d3e9642b952f78371351799649af9
Panagiotis Englezos <panagiotisegl@gmail.com>
2019-12-09 13:32:47 +0200

    Update post exercise

--- PHP/post/register.php
+++ PHP/post/register.php
@@ -1,26 +1,91 @@
+<?php
+	if(empty($_POST)){
+	header("location:sign.php");
+	}
+	?>
 <!DOCTYPE html>
 <html>
 	<head>
 	<title>
-	    
-		</title>
+	    Form
+	</title>
 		<meta charset="utf-8"/>
 	</head>
 	<body>
 	<pre>
+
+<?php 
+	//print_r($_POST); //metablith susthmatos apo tn php
+	
+	if (empty($_POST['eponimo'])){
+		echo"Δεν δώσατε επώνυμο!!";
+	}else{
+		echo $_POST['eponimo'];
+	}
+	
+	if (empty($_POST['onoma'])){
+		echo"Δεν δώσατε όνομα!!";
+	}else{
+		echo $_POST['onoma'];
+	}
+	
+	if (empty($_POST['email'])){
+		echo"Δεν δώσατε email!!";
+	}else{
+		echo $_POST['email'];
+	}
+	?>
 	<?php
-	print_r($_POST);
+	if (empty($_POST['password'])){
+		echo"Δεν δώσατε password!!";
+	}else if (empty($_POST['repassword'])){
+		echo"Δεν δωσατε κωδικο επαληθευσης!!";
+	}else if ($_POST['password']==$_POST['repassword']){
+		echo"correct password!!!";
+	}else{
+		echo"wrong password";
+	}
+?>
+<?php	
+	if (empty($_POST['email'])){
+		echo"give email please!!!";
+	}else if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){
+		echo "give correct email";
+	}else { echo $_POST['email'];
+	}
 	
 	?>
+	<?php
+	if (empty($_POST['eponimo'])){
+		echo"doste eponimo";
+	}else  if (!preg_match('/^[a-zA-Z\x{0386}-\x{03CE}\s]+$/u',$_POST['eponimo'])){
+		echo "lathos eponimo";
+	}
+	else{echo "<p>".$_POST['eponimo']."</p>";
+	}
+	?>
+	<?php 
+		$tk="12345";
+			var_dump(is_numeric($tk));
+			if (empty($_POST['TK'])){
+				echo "dose TK";
+			}else if (is_numeric($_POST['TK'])){
+				echo "<p>".$_POST['TK']."</p>";
+			}
+			else {echo "lathos TK";
+			}
+	?>
+	<?php
+	if (empty($_POST['dieuthinsh'])){
+		echo"dose dieuthinsh";
+	}else if (!preg_match('/^[a-zA-Z0-9\x{0386}-\x{03CE}\s]+$/u',$_POST['eponimo'])){
+		echo"lathos dieuthinsh";
+	}
+	else{echo "<p>".$_POST['dieuthinsh']."</p>";
+	}
+	?>
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-		</body>
+
+	</pre>
+	</body>	
 </html>
--- PHP/post/sign.php
+++ PHP/post/sign.php
@@ -1,3 +1,8 @@
+<?php
+if(!empty($_POST)){
+	print_r($_POST);
+}
+?>
 <!DOCTYPE html>
 <html>
 	<head>
@@ -5,13 +10,34 @@
 	    Form
 		</title>
 		<meta charset="utf-8"/>
+		<style>
+			.error{
+				color:red;
+				font-weight:bold;
+			}
+		</style>
 	</head>
 	<body>
 	<h1>Φόρμα Εγραφής Πελάτη</h1>
-		<form action="register.php" method="post">
+		<form action="sign.php" method="post">
 			<p>
+				<?php
+						$eponimo="";
+						if(isset($_POST['eponimo'])){
+							if(empty($_POST['eponimo'])){
+								echo '<span class="error">doste eponimo</span>';
+							}else  if (!preg_match('/^[a-zA-Z\x{0386}-\x{03CE}\s]+$/u',$_POST['eponimo'])){
+								echo '<span class="error">lathos eponimo</span>';
+								}
+									else {
+										//echo "<p>".$_POST['eponimo']."</p>";
+										$eponimo=$_POST['eponimo'];
+									}
+						}
+					?>
 				<label for="eponimo">Επώνυμο :
-					<input type="text" name="eponimo" size="25" placeholder="Επώνυμο" maxlength="35" />
+					<input type="text" name="eponimo" size="25" placeholder="Επωνυμο" value="<?=$eponimo?>" maxlength="35" />
+					
 				</label>
 			</p>
 			<p>
@@ -35,12 +61,23 @@
 				</label>
 			</p>
 			<p>
-				<label for="password">Επαλήθευση Password :<br />
-					<input type="password" name="password" />
+				<label for="repassword">Επαλήθευση Password :<br />
+					<input type="password" name="repassword" />
+				</label>
+			</p>
+			<p>
+				<label for="dieuthinsh">Διευθηνση και αριθμος :<br />
+					<input type="text" name="dieuthinsh" />
+				</label>
+			</p>
+			<p>
+				<label for="TK">TK :<br />
+					<input type="text" name="TK" />
 				</label>
 			</p>
 			
-			<button type="submit">ΕΓΓΡΑΦΗ </button>
+			
+			<button type="submit">ΕΓΓΡΑΦΉ</button>
 			
 			
 			
