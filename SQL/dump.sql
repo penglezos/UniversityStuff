fd23c291c1c6eea1de8134271d8ad6889310bc6d
Panagiotis Englezos <panagiotisegl@gmail.com>
2019-12-19 09:30:33 +0200

    Inner join esoda

--- SQL/dump.sql
+++ SQL/dump.sql
@@ -13,3 +13,12 @@ FROM exoda
 INNER JOIN exoda_kat on exoda_kat.exoda_kat_id = exoda.exoda_kat_id
 
 ORDER BY exoda_date
+
+-- INNER JOIN ON ESODA
+SELECT esoda_katathgoria.esoda_kat_onoma,
+	esoda.esoda_date,
+    esoda.esoda_poso
+FROM esoda
+INNER JOIN esoda_katathgoria on esoda_katathgoria.esoda_cat_id = esoda.esoda_kat_id
+
+ORDER BY esoda_date
