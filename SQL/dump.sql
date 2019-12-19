986a80d6cc16001a8575a2d0b946fe2039445349
Panagiotis Englezos <panagiotisegl@gmail.com>
2019-12-19 08:59:36 +0200

    ORDER BY

--- SQL/dump.sql
+++ SQL/dump.sql
@@ -1,7 +1,15 @@
+-- INNER JOIN ON TABLES
 SELECT exoda_kat.exoda_kat_name,
 	exoda.exoda_date,
     exoda.exoda_poso
 FROM exoda
 INNER JOIN exoda_kat on exoda_kat.exoda_kat_id = exoda.exoda_kat_id
 
+-- ORDER DATES --
+SELECT exoda_kat.exoda_kat_name,
+	exoda.exoda_date,
+    exoda.exoda_poso
+FROM exoda
+INNER JOIN exoda_kat on exoda_kat.exoda_kat_id = exoda.exoda_kat_id
 
+ORDER BY exoda_date
